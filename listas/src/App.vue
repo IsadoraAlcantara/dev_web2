bf7adc88c846aad81e9b3924cd8d268cb61fe59f
isadoraalcantara <isadoraalca@gmail.com>
2023-04-25 11:55:24 -0300

    lista

---
+++ listas/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref } from 'vue'
+
+const novoItem = ref('')
+const listaCompras = ref(['arroz', 'batata', 'feijão'])
+
+function adicionar() {
+  listaCompras.value.push(novoItem.value)
+  novoItem.value = ''
+}
+</script>
+
+<template>
+  <input type="text" v-model="novoItem">
+  <button @click="adicionar">Adicionar</button>
+  <ul>
+    <li v-for="item in lista">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
