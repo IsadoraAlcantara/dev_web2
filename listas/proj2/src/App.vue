2bc81c3906d5665dd6891f4b5fcddda3b358cab9
isadoraalcantara <isadoraalca@gmail.com>
2023-05-02 11:55:21 -0300

    add

--- listas/proj2/src/App.vue
+++ listas/proj2/src/App.vue
@@ -2,56 +2,68 @@
 import { ref } from 'vue'
 
 const contador = ref(0)
+
+const totalPreco = ref(0)
 const produtos = [
     {
         id: 1,
         nome: 'Camiseta',
-        preco: 49.90
+        preco: 49.90,
+        quantidade: 0
     },
     {
         id: 2,
         nome: 'Calça',
-        preco: 99.90
+        preco: 99.90,
+        quantidade: 0
     },
     {
         id: 3,
         nome: 'Meia',
-        preco: 9.90
+        preco: 9.90,
+        quantidade: 0
     },
     {
         id: 4,
         nome: 'Sapato',
-        preco: 199.90
+        preco: 199.90,
+        quantidade: 0
     },
     {
         id: 5,
         nome: 'Boné',
-        preco: 29.90
+        preco: 29.90,
+        quantidade: 0
     },
     {
         id: 6,
         nome: 'Óculos',
-        preco: 99.90
+        preco: 99.90,
+        quantidade: 0
     },
     {
         id: 7,
         nome: 'Relógio',
-        preco: 299.90
+        preco: 299.90,
+        quantidade: 0
     },
     {
         id: 8,
         nome: 'Bermuda',
-        preco: 79.90
+        preco: 79.90,
+        quantidade: 0
     },
     {
         id: 9,
         nome: 'Cueca',
-        preco: 19.90
+        preco: 19.90,
+        quantidade: 0
     },
     {
         id: 10,
         nome: 'Meia',
-        preco: 9.90
+        preco: 9.90,
+        quantidade: 0
     }
 ]
 
@@ -60,34 +72,33 @@ const carrinho = [
         id: 1,
         nome: 'Camiseta',
         preco: 49.90,
-        quantidade: 1,
+        quantidadeCarrinho: 1,
         valorTotal: 49.90
     },
     {
         id: 2,
         nome: 'Calça',
         preco: 99.90,
-        quantidade: 2,
+        quantidadeCarrinho: 2,
         valorTotal: 199.80
     },
     {
         id: 3,
         nome: 'Meia',
         preco: 9.90,
-        quantidade: 4,
+        quantidadeCarrinho: 4,
         valorTotal: 39.60
     }
 ]
 
 function total() {
     for (let i = 0; i < carrinho.length; i++) {
-        const element = valorTotal[i];
-
+        const valorTotal = valorTotal[i] + valorTotal;
     }
 }
 
 function adicionar() {
-    carrinho.value.push(produto.value)
+    carrinho.value.push(produtos.value)
     carrinho.value = ''
 }
 
@@ -99,19 +110,21 @@ function remover(key) {
 <template>
     <ul>
         <h2>Produtos</h2>
-        <li v-for="produto in produtos" :key="produto.id">
-            <p>Nome: {{ produto.name }}</p>
-            <p>Detalhes</p>
+        <li v-for="(produto, index) in produtos" :key="index">
+            <p>Nome: {{ produto.nome }}</p>
+            <p>Preço: R$:{{ produto.preco }}</p>
+            <p>Valor Total: R$:{{ produto.valorTotal }}</p>
+            <p>Quantidade: {{ produto.quantidade }}</p>
             <p v-for="(produto, key) in produtos" :key="key">{{ produto.key }}</p>
-            <button @click="adicionar">+</button>
+            <button @click="produto.quantidade++">+</button>
             <button @click="remover">-</button>
-            <p>{{ contador }}</p>
         </li>
     </ul>
 
     <div class="container">
         <h2>Carrinho</h2>
-        <p>{{ total }}</p>
+        <button @click="total" class="botao">Somar produtos</button>
+        <p>{{ valorTotal }}</p>
             <button type="submit" class="botao">finalizar compra</button>
     </div>
 </template>
