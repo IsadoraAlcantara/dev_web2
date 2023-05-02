ffe7fb34e84770230f962e45928db1d55aaa8729
isadoraalcantara <isadoraalca@gmail.com>
2023-05-02 11:20:56 -0300

    add

--- listas/proj2/src/App.vue
+++ listas/proj2/src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 
-const quantidade = ref(0)
-    const produtos = [
+const contador = ref(0)
+const produtos = [
     {
         id: 1,
         nome: 'Camiseta',
@@ -82,56 +82,65 @@ const carrinho = [
 function total() {
     for (let i = 0; i < carrinho.length; i++) {
         const element = valorTotal[i];
-        
+
     }
 }
 
 function adicionar() {
     carrinho.value.push(produto.value)
     carrinho.value = ''
-} 
+}
 
 function remover(key) {
     carrinho.value.splice(index, 1)
-  }
-} 
+}
+
 </script>
 <template>
-  <ul>
-    <li v-for="produto in produtos" :key="produto.id">
-      <p>Nome: {{ produto.name }}</p>
-      <p>Detalhes</p>
-      <p v-for="(produto, key) in produtos" :key="key">{{ produto.key }}</p>
-      <button @click="adicionar">+</button>
-      <button @click="remover">-</button>
-      <p>{{ quantidade }}</p>
-    </li>
-  </ul>
+    <ul>
+        <h2>Produtos</h2>
+        <li v-for="produto in produtos" :key="produto.id">
+            <p>Nome: {{ produto.name }}</p>
+            <p>Detalhes</p>
+            <p v-for="(produto, key) in produtos" :key="key">{{ produto.key }}</p>
+            <button @click="adicionar">+</button>
+            <button @click="remover">-</button>
+            <p>{{ contador }}</p>
+        </li>
+    </ul>
 
-  <div class="container">
-    <h2>Formulário</h2>
-  <form @submit.prevent="confirmacao = confirma()">
-    <input type="text" placeholder="nome" v-model="nome" minlength="3" required/>
-    <p></p><input type="email" placeholder="email"  v-model="email" required/>
-    <p></p><input type="password" placeholder="senha"  v-model="senha" />
-    <p></p><input type="password" placeholder="senha de confirmação"  v-model="senhaConfir" />
-    <p></p><input type="date" placeholder="data de nascimento"  v-model="nascimento" />
-    <p></p><input type="text" placeholder="endereço" v-model="endereco" />
-    <p></p><input type="text" placeholder="cidade" v-model="cidade" />
-    <select v-model="estado">
-      <option value="" disabled>Estado</option>
-      <option value="AC">Acre</option>
-    </select>
-  
-    <textarea placeholder="hobbies" v-model="hobbies" />
-    <p style="white-space: pre-line;"></p>
-
-    <textarea placeholder="biografia" v-model="biografia" />
-    <p style="white-space: pre-line;"></p>
-
-    <textarea placeholder="linguagens de programação" v-model="linguagem" />
-    <p style="white-space: pre-line;"></p>
-
-    <button type="submit" class="botao">enviar</button>
-  </form>
+    <div class="container">
+        <h2>Carrinho</h2>
+        <p>{{ total }}</p>
+            <button type="submit" class="botao">finalizar compra</button>
+    </div>
 </template>
+<style scoped>
+.botao {
+    border: none;
+    border-radius: 5px;
+    font-weight: bold;
+    font-size: 16px;
+    padding: 7px 20px;
+    background-color: #595C48;
+    color: aliceblue;
+    margin: 5px;
+    width: 220px;
+}
+.container {
+    font-size: 18px;
+    background-color: #253B35;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 80px;
+    border-radius: 15px;
+    box-shadow: 0px 10px 40px #00000056;
+    width: 400px;
+    color: aliceblue;
+  }
+
+  li {
+    list-style: none;
+  }
+</style>
